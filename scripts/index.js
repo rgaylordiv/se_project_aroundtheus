fcb31d1d73b012dba68ec717290123b4738bdc57
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-06-14 20:12:29 -0700

    added save name and bio function, used template in js to add posts

--- scripts/index.js
+++ scripts/index.js
@@ -25,14 +25,66 @@ const initialCards = [
 },
 ];
 
+/* -------------------------------------------------------------------------- */
+/*                                   Element                                  */
+/* -------------------------------------------------------------------------- */
 const editProfile = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector(".modal");
-const editClose = document.querySelector('.modal__close');
+const editClose = document.querySelector('.modal_close');
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
+const profileEditName = document.querySelector("#profile-name-input");
+const profileEditBio = document.querySelector("#profile-bio-input");
+const profileEditForm = document.querySelector(".modal");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const cardList = document.querySelector(".cards__list");
 
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+function closePopup() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               Event Handlers                               */
+/* -------------------------------------------------------------------------- */
+function handlerProfileEditSumbit (evt) {
+    evt.preventDefault();
+    profileName.textContent = profileEditName.value;
+    profileBio.textContent = profileEditBio.value;
+    closePopup();
+}
+
+function getCardElement(data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    cardTitle.textContent = data.name;
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    return cardElement;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               Event Listeners                              */
+/* -------------------------------------------------------------------------- */
 editProfile.addEventListener("click", function (){
-    profileEditModal.classList.add("modal__opened");
+    profileEditName.value = profileName.textContent;
+    profileEditBio.value = profileBio.textContent;
+    profileEditModal.classList.add("modal_opened");
 });
 
 editClose.addEventListener("click", function () {
-    profileEditModal.classList.remove("modal__opened");
+    closePopup();
+});
+
+profileEditForm.addEventListener("submit", handlerProfileEditSumbit);
+
+initialCards.forEach(function(data){
+    const cardElement = getCardElement(data);
+    cardList.append(cardElement);
 });

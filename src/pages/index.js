46cdf18babc84164053451be7ca39aed0fd97b16
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-07-28 17:50:57 -0700

    refactured and functioning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,18 @@
 import FormValidator from '../components/FormValidator.js';
 import Card from '../components/Card.js';
 import Section from '../components/Section.js';
-import Popup from '../components/Popup.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import UserInfo from '../components/UserInfo.js';
 import '../pages/index.css';
+import { initialCards, config } from '../utils/constants.js';
 
-
-const initialCards = [
-{
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-},
-{
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-},
-{
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-},
-{
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-},
-{
-    name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-},
-{
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-},
-];
-
-/* -------------------------------------------------------------------------- */
-/*                                   Element                                  */
-/* -------------------------------------------------------------------------- */
-
-const popups = document.querySelectorAll('.modal');
 const profileEditFormElement = document.querySelector('#edit-form');
 const addFormElement = document.querySelector('#add-form');
 
 /* ^^ Form Elements ^^ */
 
 const editProfile = document.querySelector(".profile__edit-button");
-const profileEditModal = document.querySelector("#edit");
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
 const profileEditName = document.querySelector("#profile-name-input");
@@ -55,7 +21,6 @@ const profileEditBio = document.querySelector("#profile-bio-input");
 /* ^^ Edit Form ^^ */
 
 const addButton = document.querySelector(".profile__add-button");
-const cardAddModal = document.querySelector("#add");
 const cardTitleInput = addFormElement.querySelector('.modal__input_type_title');
 const cardImageInput = addFormElement.querySelector('.modal__input_type_url');
 
@@ -80,12 +45,6 @@ function renderCard(data, wrapper) {
     wrapper.prepend(cardElement);
 }
 
-/*function getCardElement(cardData) {
-    const card = new Card(cardData, '#card-template', handleImageClick);
-    const cardElement = card.getView();
-    return cardElement;
-}*/
-
 const section = new Section(
     {items: initialCards, renderer: (cardData) => {
         const cardElement = getCardElement(cardData);
@@ -105,10 +64,6 @@ const popupWithImage = new PopupWithImage('#image');
 popupWithImage.setEventListeners();
 
 function handleImageClick(cardData) {
-    /*previewImage.src = link;
-    previewImage.alt = name;
-    modalTitle.textContent = name;
-    openPopup(modalImage);*/
     popupWithImage.open(cardData);
 }
 
@@ -125,12 +80,9 @@ const userInfo = new UserInfo(profileName, profileBio);
 /* -------------------------------------------------------------------------- */
 
 function handleProfileEditSumbit (userData) {
-    //const userData = userInfo.setUserInfo();
-    //profileName.textContent = userData.name;
-    //profileBio.textContent = userData.job;
     console.log(userData);
     const name = userData.name;
-    const job = userData.job;
+    const job = userData.bio;
     userInfo.setUserInfo(name, job);
     popupEditForm.close();
 }
@@ -140,29 +92,12 @@ function handleProfileAddSubmit (evt) {
     const name = cardTitleInput.value;
     const link = cardImageInput.value;
     renderCard({name, link}, cardList);
-    closePopup(cardAddModal);
+    popupAddForm.close();
 
     cardTitleInput.value = '';
     cardImageInput.value = '';
     addFormElement.reset();
 }
-
-/*function handleEscape(evt) {
-    if (evt.key === "Escape") {
-      const openedModal = document.querySelector(".modal_opened");
-      closePopup(openedModal);
-    }
-  };
-
-function openPopup(modal) {
-    modal.classList.add("modal_opened");
-    document.addEventListener("keydown",handleEscape);
-  };*/
-  
-function closePopup(modal) {
-    modal.classList.remove("modal_opened");
-    document.removeEventListener("keydown", handleEscape);
-  };
 
 /* -------------------------------------------------------------------------- */
 /*                               Event Listeners                              */
@@ -177,13 +112,6 @@ editProfile.addEventListener("click", () => {
 
 /* ^^ Edit Form Events ^^ */
 
-// Form Elements
-profileEditFormElement.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    handleProfileEditSumbit(evt);
-});
-addFormElement.addEventListener("submit", handleProfileAddSubmit);
-// ^^
 
 addButton.addEventListener("click", function() {
     popupAddForm.open();
@@ -192,19 +120,10 @@ addButton.addEventListener("click", function() {
 
 /* ^^ Add Form Events ^^ */
 
-
 /* -------------------------------------------------------------------------- */
 /*                                  Validator                                 */
 /* -------------------------------------------------------------------------- */
 
-const config = ({
-    formSelector: ".modal__form",
-    inputSelector: ".modal__form-input",
-    submitButtonSelector: ".modal__button",
-    inactiveButtonClass: "modal__button_disabled",
-    inputErrorClass: "modal__input_type_error",
-    errorClass: "modal__input-error_active"
-  });
 
   const editFormValidator = new FormValidator(config, profileEditFormElement);
   const addFormValidator = new FormValidator(config, addFormElement);
